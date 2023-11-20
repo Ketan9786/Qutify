b8564c6dff1a1aa1e0c85e5f4b7f0ba70c3aef77
Ketan Sunil Kulkarni <kketan482@gmail.com>
2023-11-20 23:07:21 +0530

    hero-qtify-takehome
    hero-qtify-takehome done

---
+++ qutify/src/Components/HeroSection/index.js
@@ -0,0 +1,9 @@
+import "./HeroSection.css"
+export default () => {
+
+    return (<div className="HeroSection">
+
+        <h4> 100 Thousand Songs, ad-free <br></br> Over thousands podcast episodes</h4>
+        <img src="vibrating-headphone.png" />
+    </div>)
+}
